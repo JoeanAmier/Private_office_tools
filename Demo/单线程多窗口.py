f11bec9a370acbbfde8d9f090d182eb617b60d13
JoeanAmiee <yonglelolu@gmail.com>
2021-11-21 09:23:01 +0800

    Updates and Optimizations

--- Demo/单线程多窗口.py
+++ Demo/单线程多窗口.py
@@ -1,29 +1,40 @@
+import time
+
 import PySimpleGUI as sg
 
-layout = [[sg.Text('输入文本'), ],
-          [sg.Input(do_not_clear=True)],
-          [sg.Text(size=(15, 1), key='A')],
+layout = [[sg.Button('开始'), ],
+          [sg.Text(size=(32, 2), key='A')],
           [sg.Button('窗口B'), sg.Button('退出')]]
 
 win_1 = sg.Window('窗口A', layout)
 
 win_2_active = False
 while True:
-    ev_1, values_1 = win_1.read(timeout=1000)
-    win_1['A'].update(values_1[0])
+    ev_1, values_1 = win_1.read(timeout=100)
     if ev_1 is None or ev_1 == '退出':
         break
 
+    elif ev_1 == '开始':
+        for cache, _ in enumerate(range(10), start=1):
+            time.sleep(1)
+            win_1['A'].update(cache)
+        cache = 0
+
     elif not win_2_active and ev_1 == '窗口B':
         win_2_active = True
-        layout2 = [[sg.Text('输入文本'), ],
-                   [sg.Input(do_not_clear=True)],
+        layout2 = [[sg.Button('开始'), ],
+                   [sg.Text(size=(32, 2), key='B')],
                    [sg.Button('退出')]]
 
         win_2 = sg.Window('窗口B', layout2)
 
     elif win_2_active:
-        ev_2, values_2 = win_2.read(timeout=1000)
+        ev_2, values_2 = win_2.read(timeout=100)
         if ev_2 is None or ev_2 == '退出':
             win_2_active = False
             win_2.close()
+        elif ev_2 == '开始':
+            for cache, _ in enumerate(range(10), start=1):
+                time.sleep(1)
+                win_2['B'].update(cache)
+            cache = 0
