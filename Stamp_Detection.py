68ff9a26e947113663bde9b7361d875f76871834
JoeanAmiee <yonglelolu@gmail.com>
2021-11-27 21:41:24 +0800

    Updates and Optimizations

--- Stamp_Detection.py
+++ Stamp_Detection.py
@@ -981,27 +981,27 @@ class GUI:
         sg.theme('SD_Theme')
 
     def run(self):
-          window = self.home()
-          if not os.path.exists(ROOT):
-                os.mkdir(ROOT)
-          else:
-                window.find_element('status').update(
-                      '程序异常，请尝试删除“cache”文件夹后重新启动程序！', text_color="red")
-          file = set()
-          task = None
-          while True:
-                event, values = window.read(timeout=100)
-                if event is None:
-                      if not os.listdir(ROOT):
-                            os.rmdir(ROOT)
-                      break
-                elif event == 'choice':
-                      window['choice'].update(disabled=True)
-                      cache = self.choice_file()
-                      if cache:
-                            [file.add(i) for i in cache]
-                            window.find_element('screen').update('\n'.join(file))
-                      window['choice'].update(disabled=False)
+        window = self.home()
+        if not os.path.exists(ROOT):
+            os.mkdir(ROOT)
+        else:
+            window.find_element('status').update(
+                '程序异常，请尝试删除“cache”文件夹后重新启动程序！', text_color="red")
+        file = set()
+        task = None
+        while True:
+            event, values = window.read(timeout=100)
+            if event is None:
+                if not os.listdir(ROOT):
+                    os.rmdir(ROOT)
+                break
+            elif event == 'choice':
+                window['choice'].update(disabled=True)
+                cache = self.choice_file()
+                if cache:
+                    [file.add(i) for i in cache]
+                    window.find_element('screen').update('\n'.join(file))
+                window['choice'].update(disabled=False)
             elif event == 'reset':
                 file = set()
                 window.find_element('screen').update('当前未选择任何待检测文件！')
@@ -1010,14 +1010,14 @@ class GUI:
                     window.find_element('status').update('未选择文件！')
                     continue
                 task = threading.Thread(
-                      target=self.deal, args=(
-                            window, values['A'], file), daemon=True)
+                    target=self.deal, args=(
+                        window, values['A'], file), daemon=True)
                 task.start()
                 window.find_element('status').update('文件检测中……')
             elif event == 'all':
                 task = threading.Thread(
-                      target=self.deal, args=(
-                            window, values['A']), daemon=True)
+                    target=self.deal, args=(
+                        window, values['A']), daemon=True)
                 task.start()
                 window.find_element('status').update('文件检测中……')
             elif event == 'info':
@@ -1029,14 +1029,14 @@ class GUI:
                 window['开始检测'].update(disabled=True)
                 window['all'].update(disabled=True)
                 sg.popup_animated(
-                      GIF,
-                      message='文件检测中',
-                      text_color='black',
-                      font=('黑体', 12),
-                      icon=ICO,
-                      background_color='white',
-                      transparent_color='white',
-                      time_between_frames=100)
+                    GIF,
+                    message='文件检测中',
+                    text_color='black',
+                    font=('黑体', 12),
+                    icon=ICO,
+                    background_color='white',
+                    transparent_color='white',
+                    time_between_frames=100)
                 task.join(timeout=0)
                 if not task.is_alive():
                     sg.popup_animated(None)
@@ -1083,98 +1083,98 @@ class GUI:
         )
 
     def deal(self, window, all_, file=None):
-          if not file:
-                pdf = os.listdir()
-                log = [self.check_pdf(i) for i in pdf if i.endswith('.pdf')]
-                self.save_log(log, all_)
-                window.find_element('status').update('当前目录全部 PDF 文件检测完成！')
-          else:
-                log = [self.check_pdf(i) for i in file]
-                self.save_log(log, all_)
-                window.find_element('status').update('当前选择文件检测完成！')
+        if not file:
+            pdf = os.listdir()
+            log = [self.check_pdf(i) for i in pdf if i.endswith('.pdf')]
+            self.save_log(log, all_)
+            window.find_element('status').update('当前目录全部 PDF 文件检测完成！')
+        else:
+            log = [self.check_pdf(i) for i in file]
+            self.save_log(log, all_)
+            window.find_element('status').update('当前选择文件检测完成！')
 
     @staticmethod
     def pdf_to_image(filename):
-          """PDF生成图片"""
-          doc = fitz.open(filename)
-          for i in range(doc.page_count):
-                page = doc[i]
-                img = page.get_pixmap(matrix=MATRIX, alpha=False)
-                img.save(os.path.join(ROOT, "%s.png" % i))
-          doc.close()
+        """PDF生成图片"""
+        doc = fitz.open(filename)
+        for i in range(doc.page_count):
+            page = doc[i]
+            img = page.get_pixmap(matrix=MATRIX, alpha=False)
+            img.save(os.path.join(ROOT, "%s.png" % i))
+        doc.close()
 
     @staticmethod
     def deal_img(num):
-          """提取红色部分"""
-          file = os.path.join(ROOT, f'{num}.png')
-          img = cv2.imdecode(np.fromfile(file, dtype=np.uint8), -1)
-          img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-          mask_1 = cv2.inRange(img_hsv, LOWER_RED_1, UPPER_RED_1)
-          mask_2 = cv2.inRange(img_hsv, LOWER_RED_2, UPPER_RED_2)
-          mask = mask_1 + mask_2
-          # print(sum(sum(i) for i in mask))  # 调试时使用
-          if sum(sum(i) for i in mask) < 150000:
-                expand = cv2.dilate(mask, EXPAND)
-                cv2.imwrite(file, expand)
-          else:
-                os.remove(file)
-                # print(f'{num + 1}未生成图像！')  # 调试时使用
+        """提取红色部分"""
+        file = os.path.join(ROOT, f'{num}.png')
+        img = cv2.imdecode(np.fromfile(file, dtype=np.uint8), -1)
+        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+        mask_1 = cv2.inRange(img_hsv, LOWER_RED_1, UPPER_RED_1)
+        mask_2 = cv2.inRange(img_hsv, LOWER_RED_2, UPPER_RED_2)
+        mask = mask_1 + mask_2
+        # print(sum(sum(i) for i in mask))  # 调试时使用
+        if sum(sum(i) for i in mask) < 150000:
+            expand = cv2.dilate(mask, EXPAND)
+            cv2.imwrite(file, expand)
+        else:
+            os.remove(file)
+            # print(f'{num + 1}未生成图像！')  # 调试时使用
 
     def read_img(self, num):
-          """识别文件"""
-          file = os.path.join(ROOT, f'{num}.png')
-          img = cv2.imread(file)
-          if img is None:
-                return None
-          gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-          os.remove(file)
-          return self.stamp_ocr(gray)
+        """识别文件"""
+        file = os.path.join(ROOT, f'{num}.png')
+        img = cv2.imread(file)
+        if img is None:
+            return None
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        os.remove(file)
+        return self.stamp_ocr(gray)
 
     @staticmethod
     def stamp_ocr(img):
-          """识别圆形"""
-          circles = cv2.HoughCircles(
-                img,
-                cv2.HOUGH_GRADIENT,
-                1,
-                10,
-                param1=200,
-                param2=10,
-                minRadius=8,
-                maxRadius=16)
-          try:
-                for _ in circles[0]:
-                      return True
-          except TypeError:
-                return False
+        """识别圆形"""
+        circles = cv2.HoughCircles(
+            img,
+            cv2.HOUGH_GRADIENT,
+            1,
+            10,
+            param1=200,
+            param2=10,
+            minRadius=8,
+            maxRadius=16)
+        try:
+            for _ in circles[0]:
+                return True
+        except TypeError:
+            return False
 
     def check_pdf(self, filename):
-          self.pdf_to_image(filename)
-          file = len(os.listdir(ROOT))
-          [self.deal_img(i) for i in range(file)]
-          result = [self.read_img(i) for i in range(file)]
-          return filename, result
+        self.pdf_to_image(filename)
+        file = len(os.listdir(ROOT))
+        [self.deal_img(i) for i in range(file)]
+        result = [self.read_img(i) for i in range(file)]
+        return filename, result
 
     @staticmethod
     def save_log(log, all_=False):
-          """保存结果数据"""
-          for i, j in log:
-                name, _ = os.path.splitext(i)
-                data = [
-                      [x + 1, str(y)]
-                      for x, y in enumerate(j)
-                      if not all_ and y in [False, None] or all_
-                ]
-                if data:
-                      data = pd.DataFrame(data, columns=['页码', '状态'])
-                      data.to_excel(f'{name}.xlsx')
-                else:
-                      data = pd.DataFrame([])
-                      data.to_excel(f'{name}_正常.xlsx')
+        """保存结果数据"""
+        for i, j in log:
+            name, _ = os.path.splitext(i)
+            data = [
+                [x + 1, str(y)]
+                for x, y in enumerate(j)
+                if not all_ and y in [False, None] or all_
+            ]
+            if data:
+                data = pd.DataFrame(data, columns=['页码', '状态'])
+                data.to_excel(f'{name}.xlsx')
+            else:
+                data = pd.DataFrame([])
+                data.to_excel(f'{name}_正常.xlsx')
 
 
 def main():
-      GUI().run()
+    GUI().run()
 
 
 if __name__ == '__main__':
