2ae038979bea8492d69b7f479435cc381502ba9a
JoeanAmiee <yonglelolu@gmail.com>
2021-12-23 23:13:28 +0800

    Updates and Optimizations

--- Abnormality_Check.py
+++ Abnormality_Check.py
@@ -1,22 +1,21 @@
 import os
+import pickle
 import webbrowser
-import time
 from multiprocessing import Process
 from multiprocessing import freeze_support
 
 import PySimpleGUI as sg
 import fitz
 import pandas as pd
-import pickle
 
 ROOT = './img/'
 
 
 class GUI:
     def __init__(self):
-        self.version = 'V0.0.1'
-        self.log = None
+        self.version = 'V0.1.0'
         self.set_theme()
+        self.log = self.welcome()
         self.main = self.home()
         self.task = None
         self.file = None
@@ -38,12 +37,27 @@ class GUI:
         sg.theme_add_new('AC_Theme', theme)
         sg.theme('AC_Theme')
 
+    def welcome(self):
+        layout = [[sg.Text('正在扫描检测记录......\n请稍等......', font=('微软雅黑', 12))], ]
+        gui = sg.Window(
+            f'异常页面检查辅助工具 {self.version}',
+            layout,
+            size=(345, 70),
+            text_justification='center',
+            element_justification='center',
+            # icon=self.ICO,
+            finalize=True)
+        log = self.check_log()
+        gui.close()
+        return log
+
     def run(self):
         if not os.path.exists(ROOT):
             os.mkdir(ROOT)
         else:
             self.main.find_element('status').update(
                 '程序异常，请尝试删除“img”文件夹后重新启动程序！', text_color="red")
+        self.main['-FILE-'].update(self.log)
         while True:
             event, values = self.main.read(timeout=100)
             if event is None:
@@ -53,23 +67,30 @@ class GUI:
                 if tip:
                     self.main.find_element('status').update(tip)
                     continue
-                self.task = Process(
-                    target=self.pdf_to_image, args=(self.pdf, self.page, values['low']),
-                    daemon=True)
+                self.task = Process(target=self.pdf_to_image, args=(
+                    self.pdf, self.page, values['low']), daemon=True)
                 self.task.start()
-                self.main.find_element('status').update('处理中...')
             elif event == 'info':
                 webbrowser.open(
                     'https://github.com/JoeanAmiee/Private_office_tools')
             if self.task:
+                sg.popup_animated(
+                    sg.DEFAULT_BASE64_LOADING_GIF,
+                    message='处理中',
+                    text_color='black',
+                    font=('黑体', 12),
+                    # icon=self.ICO,
+                    background_color='white',
+                    transparent_color='white',
+                    time_between_frames=100)
                 self.task.join(timeout=0)
                 if not self.task.is_alive():
+                    sg.popup_animated(None)
                     self.start_check(self.file, self.page)
                     self.task = None
                     self.file = None
                     self.page = None
                     self.pdf = None
-                    self.main.find_element('status').update('准备就绪')
         self.main.close()
         if not os.listdir(ROOT):
             os.rmdir(ROOT)
@@ -98,11 +119,11 @@ class GUI:
         layout = [
             [sg.Frame('', [
                 [sg.Text('异常页：', font=('微软雅黑', 12)), ],
-                [sg.Listbox(values=[], size=(10, 37), key='-PAGE-', select_mode='LISTBOX_SELECT_MODE_SINGLE'), ],
+                [sg.Listbox(values=[], size=(10, 35), key='-PAGE-'), ],
             ], border_width=0, element_justification='center'),
              sg.Frame('', [
-                 [sg.Text('预览：', font=('微软雅黑', 12)), ],
-                 [sg.Image('', key='-IMAGE-', size=(440, 630))],
+                 [sg.Text('当前页：', font=('微软雅黑', 12)), sg.Text('', key='-NOW-', font=('微软雅黑', 12))],
+                 [sg.Image('', key='-IMAGE-', size=(420, 590))],
              ], border_width=0, element_justification='center')],
             [sg.Button('正常页', key='-TRUE-', font=('微软雅黑', 12), tooltip='标记当前页为正常页！'),
              sg.Button('异常页', key='-FALSE-', font=('微软雅黑', 12), tooltip='标记当前页为异常页！'),
@@ -119,39 +140,48 @@ class GUI:
     def start_check(self, xlsx, num):
         images = os.listdir(ROOT)
         pages = [i.replace(".png", "") for i in images]
-        new_log = pages.copy()
+        images = [os.path.join(ROOT, i) for i in images]
+        remove = []
 
-        def update(gui, page, confirm=False):
-            nonlocal pages, new_log
-            pages.remove(page)
+        def update(_gui, confirm=False):
+            nonlocal images, pages, remove
             if confirm:
-                new_log.remove(page)
-            gui['-PAGE-'].update(pages)
-            if not pages:
-                gui['-IMAGE-'].update('')
+                remove.append(_gui['-NOW-'].DisplayText)
+            os.remove(f"./img/{_gui['-NOW-'].DisplayText}.png")
+            if len(pages) != 0:
+                _gui['-NOW-'].update(pages.pop())
+                _gui['-IMAGE-'].update(images.pop())
+            else:
+                _gui['-TRUE-'].update(disabled=True)
+                _gui['-FALSE-'].update(disabled=True)
 
         gui = self.check_gui()
         self.main.Hide()
         gui['-PAGE-'].update(values=pages)
+        gui['-PAGE-'].update(disabled=True)  # 暂时禁用
+        images.reverse()
+        pages.reverse()
+        gui['-NOW-'].update(pages.pop())
+        gui['-IMAGE-'].update(images.pop())
         while True:
             event, values = gui.read(timeout=100)
-            if not pages:
-                break
-            elif not values:
-                break
-            elif not values['-PAGE-']:
-                continue
-            cache = values["-PAGE-"][0]
-            gui['-IMAGE-'].update(filename=f'./img/{cache}.png')
-            if event is None:
+            if not event:
                 break
             elif event == '-TRUE-':
-                update(gui, cache, True)
+                update(gui, True)
             elif event == '-FALSE-':
-                update(gui, cache)
+                update(gui)
+            elif event == '-OVER-':
+                self.update_log(remove)
+                break
         self.main.UnHide()
-        print(pages, new_log)
+        [os.remove(os.path.join(ROOT, i)) for i in os.listdir(ROOT)]
         gui.close()
+
+    def update_log(self, new):
+        if new:
+            xlsx = self.check_xlsx(self.file, to=False)
+            print(xlsx)
 
     def choice_deal(self):
         self.file = self.choice_file()
@@ -163,7 +193,7 @@ class GUI:
                     return None
                 return '选择的文件所对应的 PDF 文件不存在！'
             return '选择的文件均为正常页，无需检查！'
-        return '未选择文件，或选择的文件均为正常页，无需检查！'
+        return '未选择文件，或者选择的文件均为正常页，无需检查！'
 
     @staticmethod
     def pdf_to_image(filename, page_list, save):
@@ -171,23 +201,28 @@ class GUI:
         doc = fitz.open(filename)
         if save:
             for i in page_list:
-                img = doc[i - 1].get_pixmap(matrix=fitz.Matrix(0.7, 0.7), alpha=False)
+                img = doc[i -
+                          1].get_pixmap(matrix=fitz.Matrix(0.7, 0.7), alpha=False)
                 img.save(os.path.join(ROOT, "%s.png" % i))
                 fitz.TOOLS.store_shrink(100)  # 清空缓存
         else:
             for i in page_list:
-                img = doc[i - 1].get_pixmap(matrix=fitz.Matrix(0.7, 0.7), alpha=False)
+                img = doc[i -
+                          1].get_pixmap(matrix=fitz.Matrix(0.7, 0.7), alpha=False)
                 img.save(os.path.join(ROOT, "%s.png" % i))
         doc.close()
-        time.sleep(5)
 
     @staticmethod
-    def check_xlsx(file):
-        xlsx = pd.read_excel(file, usecols=['页码', '状态'], dtype={'页码': int, '状态': str})
+    def check_xlsx(file, to=True):
+        xlsx = pd.read_excel(
+            file, usecols=[
+                '页码', '状态'], dtype={
+                '页码': int, '状态': str})
         if xlsx.empty:
             return None
         xlsx = xlsx[(xlsx['状态'] == 'None') | (xlsx['状态'] == 'False')]
-        xlsx = xlsx['页码'].tolist()
+        if to:
+            return xlsx['页码'].tolist()
         return xlsx
 
     def choice_file(self):
@@ -200,6 +235,17 @@ class GUI:
             # icon=self.ICO,
             no_window=True,
         )
+
+    def check_log(self):
+        log = self.get_log()
+        if not log:
+            return None
+        new_ = set()
+        for i in log:
+            j = i.replace('.pdf', '.xlsx')
+            if os.path.exists(j) and self.check_xlsx(j):
+                new_.add(i)
+        return new_
 
     @staticmethod
     def get_log():
